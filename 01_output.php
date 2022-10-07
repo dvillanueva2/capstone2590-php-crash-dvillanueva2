27b150929b37fd8be053c0cc04a201f609a38150
Dino Paolo Villanueva <dvillanueva2@nait.ca>
2022-10-07 12:02:24 -0600

    First commit - my first PHP web page

---
+++ 01_output.php
@@ -0,0 +1,19 @@
+<!-- Any PHP code that I write will go in a <?php ?> tag -->
+<!-- PHP logic is up on the top - for example to fetch something from the database and then ouput then output them down to the html -->
+<?php
+    // parses the text below to get it displayed in the browser
+    echo 'Hello Beautiful by Dino Vilanueva The Handsome';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?php echo 123; ?></h1>
+</body>
+</html>
